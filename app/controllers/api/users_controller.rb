04ca28300c2e1c7faa28fbdd53b46873c680c330
RollerCoasterRoss <rsw3020@gmail.com>
2021-03-06 11:13:17 -0600

    minor changes to controller to authenticate user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::UsersController < ApplicationController
-  before_action :authenticate_user, only: [:show]
+  before_action :authenticate_user, except: [:create]
 
   def index
     @users = User.all
